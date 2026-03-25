ae8971a25801078f467b337318747afabffc393d
hohaandrada-ux <hohaandrada@gmail.com>
2026-03-25 09:37:38 +0200

    Update test_script.py

---
+++ tema_3_tests/test_script.py
@@ -0,0 +1,27 @@
+import sys
+import os
+
+# ca să poți importa din src
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from src.service import model_function
+
+
+def test_positive():
+    result = model_function("Îmi place produsul")
+    assert "pozitiv" in result.lower()
+
+
+def test_negative():
+    result = model_function("Nu îmi place produsul")
+    assert "negativ" in result.lower()
+
+
+def test_neutral():
+    result = model_function("Produsul este ok")
+    assert "neutru" in result.lower()
+
+
+def test_empty_input():
+    result = model_function("")
+    assert result is not None
